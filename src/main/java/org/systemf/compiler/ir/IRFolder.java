40dc6de1dd8b61dc537440285085cc241a1dde39
czm23333 <34388355+czm23333@users.noreply.github.com>
2025-07-13 22:33:27 +0800

    IR: Make IRFolder a instruction visitor

--- src/main/java/org/systemf/compiler/ir/IRFolder.java
+++ src/main/java/org/systemf/compiler/ir/IRFolder.java
@@ -5,17 +5,28 @@ import org.systemf.compiler.ir.value.Value;
 import org.systemf.compiler.ir.value.constant.ConstantFloat;
 import org.systemf.compiler.ir.value.constant.ConstantInt;
 import org.systemf.compiler.ir.value.instruction.nonterminal.CompareOp;
+import org.systemf.compiler.ir.value.instruction.nonterminal.bitwise.*;
+import org.systemf.compiler.ir.value.instruction.nonterminal.conversion.FpToSi;
+import org.systemf.compiler.ir.value.instruction.nonterminal.conversion.SiToFp;
+import org.systemf.compiler.ir.value.instruction.nonterminal.farithmetic.*;
+import org.systemf.compiler.ir.value.instruction.nonterminal.iarithmetic.*;
+import org.systemf.compiler.ir.value.instruction.terminal.CondBr;
 import org.systemf.compiler.ir.value.instruction.terminal.Terminal;
 
 import java.util.Optional;
 import java.util.function.BiFunction;
 import java.util.function.Function;
 
-public class IRFolder {
+public class IRFolder extends InstructionVisitorBase<Optional<?>> {
 	public final IRBuilder builder;
 
 	public IRFolder(IRBuilder builder) {
 		this.builder = builder;
+	}
+
+	@Override
+	protected Optional<?> defaultValue() {
+		return Optional.empty();
 	}
 
 	public Optional<Value> tryFoldIntBinary(Value l, Value r, BiFunction<Long, Long, Long> fold) {
@@ -28,40 +39,90 @@ public class IRFolder {
 		return tryFoldIntBinary(lhs, rhs, (l, r) -> l & r);
 	}
 
+	@Override
+	public Optional<Value> visit(And inst) {
+		return tryFoldAnd(inst.getX(), inst.getY());
+	}
+
 	public Optional<Value> tryFoldAShr(Value lhs, Value rhs) {
 		return tryFoldIntBinary(lhs, rhs, (l, r) -> l >> r);
+	}
+
+	@Override
+	public Optional<Value> visit(AShr inst) {
+		return tryFoldAShr(inst.getX(), inst.getY());
 	}
 
 	public Optional<Value> tryFoldShl(Value lhs, Value rhs) {
 		return tryFoldIntBinary(lhs, rhs, (l, r) -> l << r);
 	}
 
+	@Override
+	public Optional<Value> visit(Shl inst) {
+		return tryFoldShl(inst.getX(), inst.getY());
+	}
+
 	public Optional<Value> tryFoldXor(Value lhs, Value rhs) {
 		return tryFoldIntBinary(lhs, rhs, (l, r) -> l ^ r);
+	}
+
+	@Override
+	public Optional<Value> visit(Xor inst) {
+		return tryFoldXor(inst.getX(), inst.getY());
 	}
 
 	public Optional<Value> tryFoldLShr(Value lhs, Value rhs) {
 		return tryFoldIntBinary(lhs, rhs, (l, r) -> l >>> r);
 	}
 
+	@Override
+	public Optional<Value> visit(LShr inst) {
+		return tryFoldLShr(inst.getX(), inst.getY());
+	}
+
 	public Optional<Value> tryFoldAdd(Value lhs, Value rhs) {
 		return tryFoldIntBinary(lhs, rhs, Long::sum);
+	}
+
+	@Override
+	public Optional<Value> visit(Add inst) {
+		return tryFoldAdd(inst.getX(), inst.getY());
 	}
 
 	public Optional<Value> tryFoldSub(Value lhs, Value rhs) {
 		return tryFoldIntBinary(lhs, rhs, (l, r) -> l - r);
 	}
 
+	@Override
+	public Optional<Value> visit(Sub inst) {
+		return tryFoldSub(inst.getX(), inst.getY());
+	}
+
 	public Optional<Value> tryFoldMul(Value lhs, Value rhs) {
 		return tryFoldIntBinary(lhs, rhs, (l, r) -> l * r);
+	}
+
+	@Override
+	public Optional<Value> visit(Mul inst) {
+		return tryFoldMul(inst.getX(), inst.getY());
 	}
 
 	public Optional<Value> tryFoldSDiv(Value lhs, Value rhs) {
 		return tryFoldIntBinary(lhs, rhs, (l, r) -> l / r);
 	}
 
+	@Override
+	public Optional<Value> visit(SDiv inst) {
+		return tryFoldSDiv(inst.getX(), inst.getY());
+	}
+
 	public Optional<Value> tryFoldSRem(Value lhs, Value rhs) {
 		return tryFoldIntBinary(lhs, rhs, (l, r) -> l % r);
+	}
+
+	@Override
+	public Optional<Value> visit(SRem inst) {
+		return tryFoldSRem(inst.getX(), inst.getY());
 	}
 
 	public Optional<Value> tryFoldICmpAtom(Value l, Value r, BiFunction<Long, Long, Boolean> fold) {
@@ -79,6 +140,11 @@ public class IRFolder {
 		};
 	}
 
+	@Override
+	public Optional<Value> visit(ICmp inst) {
+		return tryFoldICmp(inst.getX(), inst.getY(), inst.method);
+	}
+
 	public Optional<Value> tryFoldFloatBinary(Value l, Value r, BiFunction<Double, Double, Double> fold) {
 		if (l instanceof ConstantFloat lC && r instanceof ConstantFloat rC)
 			return Optional.of(builder.buildConstantFloat(fold.apply(lC.value, rC.value)));
@@ -89,16 +155,36 @@ public class IRFolder {
 		return tryFoldFloatBinary(lhs, rhs, Double::sum);
 	}
 
+	@Override
+	public Optional<Value> visit(FAdd inst) {
+		return tryFoldFAdd(inst.getX(), inst.getY());
+	}
+
 	public Optional<Value> tryFoldFMul(Value lhs, Value rhs) {
 		return tryFoldFloatBinary(lhs, rhs, (l, r) -> l * r);
+	}
+
+	@Override
+	public Optional<Value> visit(FMul inst) {
+		return tryFoldFMul(inst.getX(), inst.getY());
 	}
 
 	public Optional<Value> tryFoldFSub(Value lhs, Value rhs) {
 		return tryFoldFloatBinary(lhs, rhs, (l, r) -> l - r);
 	}
 
+	@Override
+	public Optional<Value> visit(FSub inst) {
+		return tryFoldFSub(inst.getX(), inst.getY());
+	}
+
 	public Optional<Value> tryFoldFDiv(Value lhs, Value rhs) {
 		return tryFoldFloatBinary(lhs, rhs, (l, r) -> l / r);
+	}
+
+	@Override
+	public Optional<Value> visit(FDiv inst) {
+		return tryFoldFDiv(inst.getX(), inst.getY());
 	}
 
 	public Optional<Value> tryFoldFloatUnary(Value x, Function<Double, Double> fold) {
@@ -108,6 +194,11 @@ public class IRFolder {
 
 	public Optional<Value> tryFoldFNeg(Value op) {
 		return tryFoldFloatUnary(op, x -> -x);
+	}
+
+	@Override
+	public Optional<Value> visit(FNeg inst) {
+		return tryFoldFNeg(inst.getX());
 	}
 
 	public Optional<Value> tryFoldFCmpAtom(Value l, Value r, BiFunction<Double, Double, Boolean> fold) {
@@ -127,9 +218,19 @@ public class IRFolder {
 		};
 	}
 
+	@Override
+	public Optional<Value> visit(FCmp inst) {
+		return tryFoldFCmp(inst.getX(), inst.getY(), inst.method);
+	}
+
 	public Optional<Value> tryFoldFpToSi(Value op) {
 		if (op instanceof ConstantFloat opC) return Optional.of(builder.buildConstantInt((long) opC.value));
 		return Optional.empty();
+	}
+
+	@Override
+	public Optional<Value> visit(FpToSi inst) {
+		return tryFoldFpToSi(inst.getX());
 	}
 
 	public Optional<Value> tryFoldSiToFp(Value op) {
@@ -137,10 +238,20 @@ public class IRFolder {
 		return Optional.empty();
 	}
 
+	@Override
+	public Optional<Value> visit(SiToFp inst) {
+		return tryFoldSiToFp(inst.getX());
+	}
+
 	public Optional<Terminal> tryFoldCondBr(Value condition, BasicBlock trueTarget, BasicBlock falseTarget) {
 		if (condition instanceof ConstantInt conditionC) {
 			if (conditionC.value == 0) return Optional.of(builder.buildBr(falseTarget));
 			else return Optional.of(builder.buildBr(trueTarget));
 		} else return Optional.empty();
+	}
+
+	@Override
+	public Optional<Terminal> visit(CondBr inst) {
+		return tryFoldCondBr(inst.getCondition(), inst.getTrueTarget(), inst.getFalseTarget());
 	}
 }

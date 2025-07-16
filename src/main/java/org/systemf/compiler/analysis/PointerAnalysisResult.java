2e4449f1b786e1d2787b57c0c79f9ad313eecace
czm23333 <34388355+czm23333@users.noreply.github.com>
2025-07-16 19:45:37 +0800

    Analysis: Adjust pointer analysis result methods

--- src/main/java/org/systemf/compiler/analysis/PointerAnalysisResult.java
+++ src/main/java/org/systemf/compiler/analysis/PointerAnalysisResult.java
@@ -2,20 +2,21 @@ package org.systemf.compiler.analysis;
 
 import org.systemf.compiler.ir.value.Value;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.NoSuchElementException;
+import java.util.Set;
 
 public record PointerAnalysisResult(HashMap<Value, HashSet<Value>> pointTo, HashMap<Value, HashSet<Value>> pointed) {
-	public HashSet<Value> pointTo(Value value) {
+	public Set<Value> pointTo(Value value) {
 		var res = pointTo.get(value);
-		if (res == null) throw new NoSuchElementException("No such value: " + value);
-		return res;
+		if (res == null) return Collections.emptySet();
+		return Collections.unmodifiableSet(res);
 	}
 
-	public HashSet<Value> pointedBy(Value value) {
+	public Set<Value> pointedBy(Value value) {
 		var res = pointed.get(value);
-		if (res == null) throw new NoSuchElementException("No such value: " + value);
-		return res;
+		if (res == null) return Collections.emptySet();
+		return Collections.unmodifiableSet(res);
 	}
 }

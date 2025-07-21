1fb8e4063be660f25e1e604e1e9056409539187c
czm23333 <34388355+czm23333@users.noreply.github.com>
2025-07-21 20:21:57 +0800

    Optimization: Make RemoveUnusedValue more aggressive

--- src/main/java/org/systemf/compiler/optimization/pass/RemoveUnusedValue.java
+++ src/main/java/org/systemf/compiler/optimization/pass/RemoveUnusedValue.java
@@ -1,37 +1,45 @@
 package org.systemf.compiler.optimization.pass;
 
 import org.systemf.compiler.ir.Module;
-import org.systemf.compiler.ir.block.BasicBlock;
 import org.systemf.compiler.ir.global.Function;
 import org.systemf.compiler.ir.value.Value;
+import org.systemf.compiler.ir.value.instruction.Instruction;
 import org.systemf.compiler.ir.value.instruction.PotentialSideEffect;
 import org.systemf.compiler.query.QueryManager;
 
-// TODO: Reimplement this with graph reachability to remove loops
+import java.util.ArrayDeque;
+import java.util.HashSet;
+
 public enum RemoveUnusedValue implements OptPass {
 	INSTANCE;
 
-	private boolean processBlock(BasicBlock block) {
-		boolean res = false;
-		for (var iter = block.instructions.iterator(); iter.hasNext(); ) {
-			var inst = iter.next();
-			if (!(inst instanceof Value value)) continue;
-			if (inst instanceof PotentialSideEffect) continue;
-
-			var dependant = value.getDependant();
-			var depCnt = dependant.size();
-			if (dependant.contains(value)) --depCnt;
-			if (depCnt > 0) continue;
-
-			res = true;
-			inst.unregister();
-			iter.remove();
-		}
-		return res;
-	}
-
 	private boolean processFunction(Function function) {
-		var res = function.getBlocks().stream().map(this::processBlock).reduce(false, (a, b) -> a || b);
+		var res = false;
+		var used = new HashSet<Value>();
+		var worklist = new ArrayDeque<Instruction>();
+		function.getBlocks().stream().flatMap(block -> block.instructions.stream())
+				.filter(inst -> inst instanceof PotentialSideEffect).forEach(worklist::push);
+		while (!worklist.isEmpty()) {
+			var inst = worklist.pop();
+			if (inst instanceof Value val) used.add(val);
+			inst.getDependency().stream().filter(dep -> dep instanceof Value).map(dep -> (Value) dep).forEach(val -> {
+				if (used.contains(val)) return;
+				used.add(val);
+				if (val instanceof Instruction valInst) worklist.push(valInst);
+			});
+		}
+
+		for (var block : function.getBlocks())
+			for (var iter = block.instructions.iterator(); iter.hasNext(); ) {
+				var inst = iter.next();
+				if (!(inst instanceof Value value)) continue;
+				if (inst instanceof PotentialSideEffect) continue;
+				if (used.contains(value)) continue;
+
+				res = true;
+				inst.unregister();
+				iter.remove();
+			}
 		if (res) QueryManager.getInstance().invalidateAllAttributes(function);
 		return res;
 	}

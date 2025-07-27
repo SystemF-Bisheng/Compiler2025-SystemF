f0237ab99d2751cc9b84e3e07264f53dbd3d6dfd
czm23333 <34388355+czm23333@users.noreply.github.com>
2025-07-27 18:58:12 +0800

    IR: Make PotentialSideEffect extend PotentialBlockSensitive

--- src/main/java/org/systemf/compiler/ir/value/instruction/PotentialSideEffect.java
+++ src/main/java/org/systemf/compiler/ir/value/instruction/PotentialSideEffect.java
@@ -4,5 +4,5 @@ package org.systemf.compiler.ir.value.instruction;
  * Instructions that implement this interface potentially have side effects,
  * which means that they cannot be simply removed even when unused.
  */
-public interface PotentialSideEffect extends Instruction {
+public interface PotentialSideEffect extends Instruction, PotentialBlockSensitive {
 }

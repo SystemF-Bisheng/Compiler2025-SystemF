75c345882e80a95c62abee89dd3ef8ea01f6aaf3
czm23333 <34388355+czm23333@users.noreply.github.com>
2025-08-09 15:51:21 +0800

    Optimization: Implement ReduceStrength for sdiv and srem on arbitrary I32 constant

--- src/main/java/org/systemf/compiler/optimization/pass/ReduceStrength.java
+++ src/main/java/org/systemf/compiler/optimization/pass/ReduceStrength.java
@@ -19,6 +19,7 @@ import org.systemf.compiler.ir.value.instruction.nonterminal.iarithmetic.SRem;
 import org.systemf.compiler.ir.value.util.ValueUtil;
 import org.systemf.compiler.query.QueryManager;
 import org.systemf.compiler.util.MathUtil;
+import org.systemf.compiler.util.SaturationArithmetic;
 
 import java.util.ListIterator;
 
@@ -90,6 +91,49 @@ public enum ReduceStrength implements OptPass {
 			return checkIdentity(inst, 0);
 		}
 
+		/**
+		 * @param y Shall not be zero or a power of 2, no matter positive or negative
+		 */
+		private Value handleSDiv32Constant(Value x, long y, String name) {
+			final int N = 32;
+			var ySign = y < 0;
+			var x64 = builder.buildSi32ToSi64(x, "sdivTo64");
+			var resSign = builder.buildICmp(x, builder.buildConstantZero(N), "sdivResSign",
+					ySign ? CompareOp.GT : CompareOp.LT);
+			var yAbs = Math.abs(y);
+
+			int l = 0;
+			long m;
+			for (; ; ++l) {
+				long tmp = 1L << (N - 1 + l);
+				m = tmp / yAbs + 1; // + 1 for ceiling
+				if (m * yAbs <= tmp + (1L << l)) break;
+			}
+			if (ySign) m = -m;
+
+			Value divValue;
+			if (SaturationArithmetic.isOverflow(m, N)) {
+				if (m > 0) m -= 1L << N;
+				else m += 1L << N;
+
+				var mulValue = builder.buildMul(x64, builder.buildConstantInt64(m), "sdivMul");
+				var shrValue = builder.buildAShr(mulValue, builder.buildConstantInt64(N), "sdivIAShr");
+				var shrValue32 = builder.buildSi64ToSi32(shrValue, "sdivI32AShr");
+
+				Value midValue;
+				if (m > 0) midValue = builder.buildAdd(x, shrValue32, "sdivAdd");
+				else midValue = builder.buildAdd(shrValue32, x, "sdivSub");
+
+				divValue = builder.buildAShr(midValue, builder.buildConstantInt32(l - 1), "sdivDiv");
+			} else {
+				var mulValue = builder.buildMul(x64, builder.buildConstantInt64(m), "sdivMul");
+				var shrValue = builder.buildAShr(mulValue, builder.buildConstantInt64(N - 1 + l), "sdivAShr");
+				divValue = builder.buildSi64ToSi32(shrValue, "sdivDiv");
+			}
+
+			return builder.buildAdd(divValue, resSign, name);
+		}
+
 		@Override
 		public Boolean visit(SDiv inst) {
 			if (checkIdentity(inst, 1)) return true;
@@ -98,10 +142,13 @@ public enum ReduceStrength implements OptPass {
 			if (!ValueUtil.isConstantInt(y)) return false;
 
 			var yVal = ValueUtil.getConstantInt(y);
+			if (yVal == 0) return false;
 			var yAbs = Math.abs(yVal);
 			var yPow = MathUtil.checkPowerOfTwo(yAbs);
 			var x = inst.getX();
 			var width = ValueUtil.getWidth(inst);
+			var name = inst.getName();
+
 			if (yPow != -1) {
 				builder.setPosition(iterator);
 				var xNeg = builder.buildICmp(x, builder.buildConstantZero(width), "sdivSign", CompareOp.LT);
@@ -110,14 +157,15 @@ public enum ReduceStrength implements OptPass {
 				Value newValue;
 				if (yVal < 0) {
 					var shrValue = builder.buildAShr(addValue, builder.buildConstantInt(yPow, width), "sdivAShr");
-					newValue = builder.buildSub(builder.buildConstantZero(width), shrValue, inst.getName());
-				} else newValue = builder.buildAShr(addValue, builder.buildConstantInt(yPow, width), inst.getName());
+					newValue = builder.buildSub(builder.buildConstantZero(width), shrValue, name);
+				} else newValue = builder.buildAShr(addValue, builder.buildConstantInt(yPow, width), name);
 				inst.replaceAllUsage(newValue);
-				return true;
 			} else {
-				// TODO: Any constant sdiv
-				return false;
+				if (width != 32) return false;
+				builder.setPosition(iterator);
+				inst.replaceAllUsage(handleSDiv32Constant(x, yVal, name));
 			}
+			return true;
 		}
 
 		@Override
@@ -125,23 +173,31 @@ public enum ReduceStrength implements OptPass {
 			var y = inst.getY();
 			if (!ValueUtil.isConstantInt(y)) return false;
 
-			var yAbs = Math.abs(ValueUtil.getConstantInt(y));
+			var yVal = ValueUtil.getConstantInt(y);
+			if (yVal == 0) return false;
+			var yAbs = Math.abs(yVal);
 			var yPow = MathUtil.checkPowerOfTwo(yAbs);
 			var x = inst.getX();
 			var width = ValueUtil.getWidth(inst);
+			var name = inst.getName();
+
 			if (yPow != -1) {
 				builder.setPosition(iterator);
 				var xNeg = builder.buildICmp(x, builder.buildConstantZero(width), "sremSign", CompareOp.LT);
 				var toAdd = builder.buildMul(xNeg, builder.buildConstantInt(yAbs - 1, width), "sremToAdd");
 				var addValue = builder.buildAdd(x, toAdd, "sremAdd");
 				var andValue = builder.buildAnd(addValue, builder.buildConstantInt(-yAbs, width), "sremAnd");
-				var subValue = builder.buildSub(x, andValue, inst.getName());
+				var subValue = builder.buildSub(x, andValue, name);
 				inst.replaceAllUsage(subValue);
-				return true;
 			} else {
-				// TODO: Any constant srem
-				return false;
+				if (width != 32) return false;
+				builder.setPosition(iterator);
+				var divValue = handleSDiv32Constant(x, yVal, "sremDiv");
+				var toSub = builder.buildMul(divValue, y, "sremToSub");
+				var subValue = builder.buildSub(x, toSub, name);
+				inst.replaceAllUsage(subValue);
 			}
+			return true;
 		}
 
 		@Override
@@ -174,10 +230,10 @@ public enum ReduceStrength implements OptPass {
 			var power = MathUtil.checkPowerOfTwo(yAbs);
 			if (power == -1) return false;
 
+			var name = inst.getName();
 			builder.setPosition(iterator);
-			Value newVal = builder.buildShl(inst.getX(), builder.buildConstantInt(power, width),
-					inst.getName() + "Shl");
-			if (yVal < 0) newVal = builder.buildSub(builder.buildConstantZero(width), newVal, inst.getName());
+			Value newVal = builder.buildShl(inst.getX(), builder.buildConstantInt(power, width), name + "Shl");
+			if (yVal < 0) newVal = builder.buildSub(builder.buildConstantZero(width), newVal, name);
 			inst.replaceAllUsage(newVal);
 			return true;
 		}

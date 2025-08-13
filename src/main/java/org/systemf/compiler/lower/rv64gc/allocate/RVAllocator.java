23f269123e0e4d44a06a65f5ac082f73d087930a
czm23333 <34388355+czm23333@users.noreply.github.com>
2025-08-14 00:01:46 +0800

    LIR: Remove RVSimplifyParMove because it's incorrect

--- src/main/java/org/systemf/compiler/lower/rv64gc/allocate/RVAllocator.java
+++ src/main/java/org/systemf/compiler/lower/rv64gc/allocate/RVAllocator.java
@@ -2,24 +2,12 @@ package org.systemf.compiler.lower.rv64gc.allocate;
 
 import org.systemf.compiler.lower.rv64gc.allocate.pass.RVInBlockSchedule;
 import org.systemf.compiler.lower.rv64gc.allocate.pass.RVRegAlloc;
-import org.systemf.compiler.lower.rv64gc.allocate.pass.RVSimplifyParMove;
-import org.systemf.compiler.lower.rv64gc.module.RVModule;
 import org.systemf.compiler.lower.rv64gc.optimization.RVOptimizedResult;
-import org.systemf.compiler.lower.rv64gc.optimization.pass.RVMergeChain;
-import org.systemf.compiler.lower.rv64gc.optimization.pass.RVRemoveSingleBr;
 import org.systemf.compiler.query.EntityProvider;
 import org.systemf.compiler.query.QueryManager;
 
 public enum RVAllocator implements EntityProvider<RVAllocatedResult> {
 	INSTANCE;
-
-	private void cleanUp(RVModule module) {
-		boolean flag = true;
-		while (flag) {
-			flag = RVRemoveSingleBr.INSTANCE.run(module);
-			flag |= RVMergeChain.INSTANCE.run(module);
-		}
-	}
 
 	@Override
 	public RVAllocatedResult produce() {
@@ -30,8 +18,6 @@ public enum RVAllocator implements EntityProvider<RVAllocatedResult> {
 
 		RVInBlockSchedule.INSTANCE.run(module);
 		RVRegAlloc.INSTANCE.run(module);
-		RVSimplifyParMove.INSTANCE.run(module);
-		cleanUp(module);
 
 		return new RVAllocatedResult(module);
 	}
